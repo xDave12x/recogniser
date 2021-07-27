62d08eb8477b8127a3eca32b803ea02023025b47
xDave12x <77081815+xDave12x@users.noreply.github.com>
2021-07-27 18:43:58 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -16,3 +16,16 @@ classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/a
 function modelLoaded(){
     console.log('model loaded');
 }
+function check(){
+    img=document.getElementById('captured_image');
+    classifier.classify(img,gotResult);
+}
+function gotResult(error,results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("result_object_name").innerHTML=results[0].label;
+        document.getElementById("result_object_accuracy").innerHTML=results[0].confidence.toFixed(3);
+    }
+}
